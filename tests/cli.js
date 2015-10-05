7daede6c55bf1eb00adad49d0a730c9c5f122f95
davemackintosh <me@davemackintosh.co.uk>
2015-10-05 16:11:14 +0100

    Mo tests

--- tests/cli.js
+++ tests/cli.js
@@ -2,12 +2,21 @@
 
 // Get the testing library.
 const tape = require("tape")
+const Promise = require("bluebird")
 
 // Get Multicolour.
 const Multicolour = require("../index.js")
 
 // Where we keep the test content.
 const test_content_path = "./tests/test_content/"
+
+// Used in a few tests below.
+class Server {
+  constructor(blueprints, api_config, stash) {}
+  warn() { return this }
+  start(callback) { callback && callback(); return this }
+  stop(callback) { callback && callback(); return this }
+}
 
 tape("CLI initializes an interface", test => {
   // Create an instance of multicolour.
@@ -26,7 +35,7 @@ tape("CLI initializes an interface", test => {
 })
 
 tape("CLI scope should change when using `.scope()`", test => {
-  test.plan(1)
+  test.plan(2)
 
   // Create an instance of multicolour.
   const multicolour = new Multicolour({ content: test_content_path })
@@ -38,4 +47,45 @@ tape("CLI scope should change when using `.scope()`", test => {
     .scope(alt_multicolour)
 
   test.deepEquals(cli.__scope, alt_multicolour, "Scope changed to alternative instance of multicolour.")
+  test.throws(() => cli.scope(), ReferenceError, "Should throw ReferenceError when no scope provided.")
+
+  multicolour.reset()
+})
+
+tape("CLI start and stop", test => {
+  // Create an instance of multicolour.
+  const multicolour = new Multicolour({ content: test_content_path })
+  const server_plugin = {
+    type: multicolour.request("types").SERVER_GENERATOR,
+    id: multicolour.request("uuid"),
+    generator: Server
+  }
+
+  // Set up multicolour.
+  multicolour
+    // Scan for content.
+    .scan()
+    // Use the server plugin.
+    .use(server_plugin)
+
+  // Get the CLI
+  let cli = multicolour.cli()
+
+  // Don't ever do this, just for tests.
+  delete cli.__scope
+
+  test.throws(() => cli.start(), ReferenceError, "Start should throw when no __scope present and server start fired.")
+  test.throws(() => cli.stop(), ReferenceError, "Stop should throw when no __scope present and server stop fired.")
+
+  // Re-set the scope.
+  cli = cli.scope(multicolour)
+
+  // Make sure nothing throws here.
+  cli.start(cli.program, { config: `${test_content_path}/config.js` })
+  cli.stop()
+
+  // Reset.
+  multicolour.reset()
+
+  test.end()
 })
