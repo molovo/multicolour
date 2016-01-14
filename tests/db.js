5c1f72557d2e27cc8c6c1d359a9b31c7e6b62997
davemackintosh <me@davemackintosh.co.uk>
2016-01-14 14:27:49 +0000

    Coverage back up to 100%.
    Signed-off-by: davemackintosh <me@davemackintosh.co.uk>

--- tests/db.js
+++ tests/db.js
@@ -10,15 +10,56 @@ const Multicolour = require("../index.js")
 const test_content_path = "./tests/test_content/"
 
 tape("Waterline collections are created by Multicolour on instantiation and we only get expected errors.", test => {
-  test.plan(4)
+  test.plan(12)
 
   // Create an instance of multicolour.
-  const multicolour = Multicolour
-    .new_from_config_file_path(`${test_content_path}config.js`)
-    .scan()
+  const multicolour = new Multicolour({
+    content: test_content_path,
+    db: {
+      adapters: {
+        development: require("sails-memory")
+      },
+      connections: {
+        development: {
+          adapter: "development"
+        }
+      }
+    }
+  }).scan()
 
   test.notEquals(typeof multicolour.get("blueprints"), "undefined", "Blueprints exists")
   test.notEquals(typeof multicolour.get("database"), "undefined", "Database exists")
+
+  // Seed for some more tests.
+  multicolour.get("database").start(ontology => {
+    const models = ontology.collections
+
+    models.test.create({ name: "test", age: 100, empty: null }, (err, t) => {
+      test.equal(err, null, "No error during 1st seed")
+      test.doesNotThrow(() => t.toJSON(), "Called toJSON on test")
+    })
+    models.test2.create({ name: "test", age: 100 }, (err, t) => {
+      test.equal(err, null, "No error during 2nd seed")
+      test.doesNotThrow(() => t.toJSON(), "Called toJSON on test2")
+    })
+    models.user.create({
+      username: "test",
+      name: "test",
+      password: "password"
+    }, (err, user) => {
+      test.equal(err, null, "No error during 2nd seed")
+      test.doesNotThrow(() => user.toJSON(), "Called toJSON on user")
+    })
+    models.user.create({
+      username: "test2",
+      name: "test2",
+      email: null
+    }, (err, user) => {
+      test.equal(err, null, "No error during 2nd seed")
+      test.doesNotThrow(() => user.toJSON(), "Called toJSON on user without password")
+    })
+  })
+
   test.throws(() => multicolour.get("database").start(), TypeError, "Should throw without a callback.")
 
   delete multicolour.get("config").delete("db")
--- tests/test_content/blueprints/test.js
+++ tests/test_content/blueprints/test.js
@@ -11,6 +11,11 @@ module.exports = {
       type: "integer",
       min: 0,
       max: 9000 // OVER 9000!???!!!
+    },
+    empty: "string",
+
+    toJSON: function() {
+      return this.toObject()
     }
   }
 }
