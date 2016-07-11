e7df128fe05d88cbe5c3a09139e2906ee17f4152
davemackintosh <me@davemackintosh.co.uk>
2016-07-11 17:42:33 +0100

    Unit Tests :: Trying to get to the bottom of the failing tests.
    Waterline keeps telling me something is already registered, it's the development connection identity but it's not registered.. Not yet? 😫
    
    Signed-off-by: davemackintosh <me@davemackintosh.co.uk>

--- tests/db.js
+++ tests/db.js
@@ -10,38 +10,45 @@ const Multicolour = require("../index")
 const test_content_path = "./tests/test_content/"
 
 tape("Waterline collections are created by Multicolour on instantiation and we only get expected errors.", test => {
-  test.plan(17)
+  test.plan(18)
 
   // Create an instance of multicolour.
-  const multicolour = new Multicolour({
-    content: test_content_path,
-    db: {
-      adapters: {
-        development: require("sails-memory")
-      },
-      connections: {
-        development: {
-          adapter: "development"
-        }
-      }
-    }
-  }).scan()
+  const multicolour = Multicolour.new_from_config_file_path(test_content_path + "config.js").scan()
+  const DB = multicolour.get("database")
 
   // Test stuff exists.
   test.notEquals(typeof multicolour.get("blueprints"), "undefined", "Blueprints exists")
-  test.notEquals(typeof multicolour.get("database"), "undefined", "Database exists")
+  test.notEquals(typeof DB, "undefined", "Database connection exists")
 
   // Test forceful relationship editing.
-  test.doesNotThrow(() => multicolour.get("database").add_relation_to_collection("test", "test2", "test2"), "Does not throw when forcefully extending collection.")
-  test.doesNotThrow(() => multicolour.get("database").add_relation_to_collection("test", "testing", "test2", true), "Does not throw when forcefully extending collection with multiple.")
-  test.throws(() => multicolour.get("database").add_relation_to_collection("fake", "test2", "test2"), ReferenceError, "Throws when source collection not found.")
-  test.throws(() => multicolour.get("database").add_relation_to_collection("test", "fake", "fake"), ReferenceError, "Throws when target collection not found.")
-  test.throws(() => multicolour.get("database").add_relation_to_collection("test", "test2", "test2"), TypeError, "Throws when trying to overwrite existing relationship.")
+  // One to One
+  test.doesNotThrow(() => DB.add_relation_to_collection("one2one", "test", "test2"), "Does not throw when forcefully extending collection.")
+
+  // One to Many
+  test.doesNotThrow(() => DB.add_relation_to_collection("one2many", "test", "test2", true), "Does not throw when forcefully extending collection as a collection.")
+
+  // as a "join", is actually just a pretty way to add_relation_to_collection.
+  test.doesNotThrow(() => DB.join({
+    as: "join",
+    from: "test2",
+    to: "test3",
+    many: true
+  }))
+
+  // Check error behaviour.
+  test.throws(() => DB.add_relation_to_collection("fake1", "fake", "test2"), ReferenceError, "Throws when source collection not found.")
+  test.throws(() => DB.add_relation_to_collection("fake2", "test", "fake"), ReferenceError, "Throws when target collection not found.")
+  test.throws(() => DB.add_relation_to_collection("one2one", "test", "test2"), TypeError, "Throws when trying to overwrite existing relationship.")
 
   // Seed for some more tests.
-  multicolour.get("database").start(ontology => {
+  DB.start((err, ontology) => {
+    test.equal(typeof err, "undefined", "Error in starting DB is undefined.")
+    console.log(err, DB.request("waterline"));
+    if (err) return false
+
     const models = ontology.collections
 
+    // Test various inserts.
     models.test.create({ name: "test", age: 100, empty: null, test2: 1 }, (err, t) => {
       test.equal(err, null, "No error during 1st seed")
       test.doesNotThrow(() => t.toJSON(), "Called toJSON on test")
@@ -67,12 +74,4 @@ tape("Waterline collections are created by Multicolour on instantiation and we o
       test.doesNotThrow(() => user.toJSON(), "Called toJSON on user without password")
     })
   })
-
-  test.throws(() => multicolour.get("database").start(), TypeError, "Should throw without a callback.")
-
-  delete multicolour.get("config").delete("db")
-  test.throws(() => multicolour.get("database").start(err => {throw err}), Error, "Should throw without proper config.")
-
-  // Reset Multicolour
-  multicolour.reset()
 })
