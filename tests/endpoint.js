7b4e1d6286d447908857924f0f3e31ec9476d5f6
davemackintosh <me@davemackintosh.co.uk>
2016-07-19 09:47:55 +0100

    test coverage :: Added unit tests for new Endpoint library.
    Signed-off-by: davemackintosh <me@davemackintosh.co.uk>

---
+++ tests/endpoint.js
@@ -0,0 +1,36 @@
+"use strict"
+
+// Get the testing library.
+const tape = require("tape")
+
+// Get Multicolour.
+const {Endpoint} = require("../index")
+
+tape("Endpoint library", test => {
+
+  const endpoint = new Endpoint({ test: "string" })
+    .add_create_route()
+    .add_read_route()
+    .add_update_route()
+    .add_update_or_create_route()
+    .add_delete_route()
+    .add_create_frontend()
+    .add_read_frontend()
+    .add_update_frontend()
+    .add_delete_frontend()
+
+  test.throws(() => new Endpoint(), TypeError, "Throws when no blueprint passed to Endpoint")
+
+  test.equals(endpoint.POST, true, "Create route function worked")
+  test.equals(endpoint.GET, true, "Read route function worked")
+  test.equals(endpoint.PATCH, true, "Update route function worked")
+  test.equals(endpoint.DELETE, true, "Delete route function worked")
+  test.equals(endpoint.PUT, true, "Update or create route function worked")
+
+  test.equals(endpoint.FE_POST, true, "Create route function worked")
+  test.equals(endpoint.FE_GET, true, "Read route function worked")
+  test.equals(endpoint.FE_PATCH, true, "Update route function worked")
+  test.equals(endpoint.FE_DELETE, true, "Delete route function worked")
+
+  test.end()
+})
