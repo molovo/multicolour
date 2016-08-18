e470b9506f2149530f772fa7d0e7e3cbb7998dce
davemackintosh <me@davemackintosh.co.uk>
2016-08-18 21:54:30 +0100

    More tests.
    Signed-off-by: davemackintosh <me@davemackintosh.co.uk>

--- tests/handlers.js
+++ tests/handlers.js
@@ -6,6 +6,7 @@ const Async = require("async")
 
 // Get Multicolour.
 const Multicolour = require("../index")
+const Http_Error = require("../lib/http-error")
 
 // Where we keep the test content.
 const test_content_path = "./tests/test_content/"
@@ -14,8 +15,6 @@ const test_content_path = "./tests/test_content/"
 const multicolour = Multicolour.new_from_config_file_path(test_content_path + "config.js").scan()
 
 tape("Handlers", test => {
-  test.plan(11)
-
   multicolour.start(() => {
     const model = multicolour.get("database").get("models").test
     const handlers = Multicolour.handlers
@@ -25,6 +24,10 @@ tape("Handlers", test => {
     test.doesNotThrow(() => handlers.set_host(multicolour), "Doesn't throw error when setting host for handlers.")
     test.doesNotThrow(() => handlers.collection_has_associations(model), "Doesn't throw error while checking for associations on a collection.")
     test.deepEquals(handlers.compile_constraints({ payload: { user: 1 } }, constraints), { user: 1 }, "Doesn't throw error while compiling constraints")
+    test.deepEquals(handlers.compile_constraints({ payload: { user: 1 } }, null), {}, "Exits with no constraints passed in.")
+    test.deepEquals(handlers.compile_constraints({ payload: { user: 1 } }, {user: () => 1}), {user: 1}, "Executes the function in a constraint.")
+    test.deepEquals(handlers.compile_constraints({ payload: { user: 1 } }, {user: {compile: false, value: 1}}), {user: 1}, "Executes and non-compilable a constraint.")
+    test.deepEquals(handlers.compile_constraints({ payload: { user: 1 } }, {user: {compile: true, value: "payload.user"}}), {user: 1}, "Executes and compiles a constraint.")
 
     const url = {query: {}}
     const payload = { name: "test", age: 12 }
@@ -54,9 +57,22 @@ tape("Handlers", test => {
         next()
       }),
       next => handlers.DELETE.call(model, { params: { id:1 }, url }, err => {
-        test.equal(err, null, "No error in PATCH handler with id.")
+        test.equal(err, null, "No error in DELETE handler with id.")
+        next()
+      }),
+      next => handlers.DELETE.call(model, { params: { id: 999 }, url }, err => {
+        test.ok(err instanceof Http_Error, "Get an error which is a HTTP document gone status.")
+        test.equal(err.code, 410, "Document gone status code.")
+        next()
+      }),
+      next => handlers.PUT.call(model, { params: { id:2 }, payload, url }, err => {
+        test.equal(err, null, "No error in PUT handler with id.")
+        next()
+      }),
+      next => handlers.UPLOAD.call(model, { params: { id:2 }, payload: {file: test_content_path + "circle.svg"}, url }, err => {
+        test.equal(err, null, "No error in UPLOAD handler with id.")
         next()
       })
-    ], multicolour.stop.bind(multicolour))
+    ], multicolour.stop.bind(multicolour, test.end.bind(test)))
   })
 })
