b8f154abfcef3645e8e2563cb9e4cc0ca9bb4f03
davemackintosh <me@davemackintosh.co.uk>
2016-12-07 13:52:36 +0000

    Fix test suite, heading into integration testing now.

--- tests/handlers.js
+++ tests/handlers.js
@@ -17,11 +17,10 @@ const multicolour = Multicolour.new_from_config_file_path(test_content_path + "c
 tape("Handlers", test => {
   multicolour.start(() => {
     const model = multicolour.get("database").get("models").test
-    const handlers = Multicolour.handlers
+    const handlers = multicolour.get("handlers")
 
     const constraints = {user: "payload.user"}
 
-    test.doesNotThrow(() => handlers.set_host(multicolour), "Doesn't throw error when setting host for handlers.")
     test.doesNotThrow(() => handlers.collection_has_associations(model), "Doesn't throw error while checking for associations on a collection.")
     handlers.compile_constraints({payload: {user: 1}}, constraints)
     test.deepEquals(handlers.compile_constraints({payload: {user: 1}}, constraints), {user: 1}, "Doesn't throw error while compiling constraints")
@@ -30,7 +29,7 @@ tape("Handlers", test => {
     const payload = {name: "test", age: 12}
 
     Async.series([
-      next => handlers.POST.call(model, {
+      next => handlers.POST(model, {
         payload,
         params: {},
         url
@@ -39,43 +38,43 @@ tape("Handlers", test => {
         test.equal(created.length, 1, "Returned 1 created document")
         next()
       }),
-      next => handlers.GET.call(model, {params: {}, url}, (err, rows) => {
+      next => handlers.GET(model, {params: {}, url}, (err, rows) => {
         test.equal(err, null, "No error in empty GET handler")
         test.equal(rows.length, 1, "Returned 1 document")
         next()
       }),
-      next => handlers.GET.call(model, {params: {id: 1}, url}, (err, rows) => {
+      next => handlers.GET(model, {params: {id: 1}, url}, (err, rows) => {
         test.equal(err, null, "No error in GET handler with id.")
         test.equal(rows.length, 1, "Returned 1 document")
         next()
       }),
-      next => handlers.GET.call(model, {params: {id: 999}, url}, err => {
+      next => handlers.GET(model, {params: {id: 999}, url}, err => {
         test.ok(err, "Expected error in GET handler with bad id.")
         test.equal(err.code, 404, "Returned a 404")
         next()
       }),
-      next => handlers.PATCH.call(model, {payload, params: {id: 1}, url}, err => {
+      next => handlers.PATCH(model, {payload, params: {id: 1}, url}, err => {
         test.equal(err, null, "No error in PATCH handler with id.")
         next()
       }),
-      next => handlers.DELETE.call(model, {params: {id: 1}, url}, err => {
+      next => handlers.DELETE(model, {params: {id: 1}, url}, err => {
         test.equal(err, null, "No error in DELETE handler with id.")
         next()
       }),
-      next => handlers.DELETE.call(model, {params: {id: 999}, url}, err => {
+      next => handlers.DELETE(model, {params: {id: 999}, url}, err => {
         test.ok(err instanceof Http_Error, "Get an error which is a HTTP document gone status.")
         test.equal(err.code, 410, "Document gone status code.")
         next()
       }),
-      next => handlers.PUT.call(model, {params: {id: 2}, payload, url}, err => {
+      next => handlers.PUT(model, {params: {id: 2}, payload, url}, err => {
         test.equal(err, null, "No error in PUT handler with id.")
         next()
       }),
-      next => handlers.PUT.call(model, {params: {id: 2}, payload, url}, err => {
+      next => handlers.PUT(model, {params: {id: 2}, payload, url}, err => {
         test.equal(err, null, "No error in PUT handler with newly created record.")
         next()
       }),
-      next => handlers.UPLOAD.call(model, {
+      next => handlers.UPLOAD(model, {
         params: {id: 999},
         payload: {
           file: {
@@ -87,7 +86,7 @@ tape("Handlers", test => {
           test.equal(err.code, 404, "Get a 404 for trying to upload to unknown row")
           next()
         }),
-      next => handlers.UPLOAD.call(model, {
+      next => handlers.UPLOAD(model, {
         params: {id: 2},
         payload: {
           file: {
