addd3fd61f05c22fbdd1935115b71842c20135a6
davemackintosh <me@davemackintosh.co.uk>
2017-02-10 20:09:05 +0000

    Improved default content

--- init/content/blueprints/example.js
+++ init/content/blueprints/example.js
@@ -1,6 +1,19 @@
 "use strict"
 
-const utils = require("multicolour/lib/utils")
+/**
+ * This is an example blueprint, it features
+ * some of the basic functionality you might
+ * want to implement yourself, in your own API.
+ *
+ * Feel free to delete this file and create your
+ * own blueprints.
+ *
+ * We (Dave, Joe and Jamie) hope you enjoy using
+ * Multicolour to make your APIs easier.
+ *
+ * If you need any help, join the Slack channel:
+ *   https://slack.getmulticolour.com/ ❤
+ */
 
 module.exports = {
   attributes: {
@@ -22,13 +35,26 @@ module.exports = {
     salt: "string",
   },
 
-  // Before we create anything, make sure
-  // to hash the password for security.
+  /**
+   * An example, lifecyle callback.
+   *
+   * This example takes the values passed in,
+   * checks if it has a password present and it then
+   * generates a new salt and hashes the password before
+   * continueing with the write operation.
+   *
+   * @param  {Object} values passed in to create.
+   * @param  {Function} next function in the queue to run.
+   * @return {void}
+   */
   beforeCreate: (values, next) => {
+
     // If no password was provided, just move on and exit.
     if (!values.password) {
       return next()
     }
+
+    const utils = require("multicolour/lib/utils")
 
     // Create a salt for this user if they don't have one.
     const salt = utils.create_salt()
