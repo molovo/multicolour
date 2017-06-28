61a570d0daef8db8c958eb0c8b1ced53b1fc5f25
davemackintosh <me@davemackintosh.co.uk>
2017-06-28 21:12:00 +0100

    Change test loading strategy to help debugging Travis weirdness...

---
+++ tests/all.js
@@ -0,0 +1,14 @@
+"use strict"
+
+Error.stackTraceLimit = Infinity
+
+require("./base")
+require("./cli")
+require("./config")
+require("./constraints")
+require("./db")
+require("./endpoint")
+require("./handlers")
+require("./http-error")
+require("./http-server")
+require("./storage")
